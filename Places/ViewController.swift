e10c78cdf2ed8fb656012d62c428351ea0f4eb13
eironeia <acuellor.6@gmail.com>
2020-05-12 13:50:47 +0200

    Project creation and assignment constraints (iOS 11.0+, only iPhone, landscape disabled for now)

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Alex Cuello Ortiz on 12/05/2020.
+//  Copyright © 2020 Chama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
